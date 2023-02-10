d737b6a0cf7f67158ed9f67f8250a3e66d56215a
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2023-02-10 11:05:06 +0900

    現在地の取得完了

---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue"
+import VueGeolocation from "vue3-geolocation"
+
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      current_location: {}, // 現在地を取得するオブジェクト
+    }
+  },
+  methods: {
+    getLocation: async function () {
+      await VueGeolocation.getLocation()
+        .then((coordinates) => {
+          this.current_location = {
+            lat: coordinates.lat, //緯度
+            lng: coordinates.lng, //経度
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+  },
+}
+</script>
