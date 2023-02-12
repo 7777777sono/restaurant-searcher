ba8da200c03311db1d75454808ca150f1574f930
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2023-02-12 21:35:57 +0900

    店舗の情報の表示完了

---
+++ src/views/DetailView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="restaurant">
+    <h3>{{ restaurant.info.name }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    restaurant: {
+      type: Object,
+    },
+  },
+
+  data() {
+    return {}
+  },
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,26 +7,35 @@
       max="3"
       min="0"
     />
-    <button v-on:click="searchResturant">検索</button>
+    <button v-on:click="search">検索</button>
+  </div>
+  <div v-if="results.length > 0" class="results-zone">
+    <div
+      v-for="(result, index) in results"
+      v-bind:key="index"
+      v-on:click="screenTransition(index)"
+      class="shop-zone"
+    >
+      <img v-bind:src="result.info.photo.pc.l" />
+      <h3>{{ result.info.name }}</h3>
+      <h3>{{ result.info.mobile_access }}</h3>
+    </div>
   </div>
 </template>
 
 <script>
-import VueGeolocation from "vue3-geolocation"
-
 export default {
+  props: {
+    results: {
+      type: Array,
+    },
+  },
+
   data() {
     return {
-      API_KEY: "3ea416f7ceb2e235", // HOTPPEPERのAPIキー
-      current_location: {}, // 現在地を取得するオブジェクト
-      url: "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=", // APIのURL(APIキー前までの)
       radius: 1, // 検索半径(km単位)
       radiusMin: 0, // 検索半径の最小値
       radiusMax: 3, // 検索半径の最大値
-      searchRange: 1, // HOTPPEPER APIの検索範囲(ある地点からの範囲内のお店の検索)を決める変数
-      format: "json", // レスポンス形式の指定
-      restaurants: [], // 取得した店の情報を格納する
-      results: [], // 検索結果を格納する連想配列
     }
   },
   methods: {
@@ -38,102 +47,75 @@ export default {
         this.radius = this.radiusMax
       }
     },
-    // 現在地を取得する関数
-    getLocation: async function () {
-      await VueGeolocation.getLocation()
-        .then((coordinates) => {
-          this.current_location = {
-            lat: coordinates.lat, //緯度
-            lng: coordinates.lng, //経度
-          }
-        })
-        .catch((error) => {
-          console.log(error)
-        })
+    // 検索を行うための関数
+    // App.vueにsearchというイベントを発火し、App.vueで店の情報の取得を行う。
+    search: function () {
+      this.$emit("search", this.radius)
     },
-    // 店の情報を取得する関数
-    getRestaurant: async function () {
-      // 検索半径300m以内なら
-      if (this.radius * 1000 <= 300) {
-        this.searchRange = 1
-      }
-      // 検索半径500m以内なら
-      else if (this.radius * 1000 <= 500) {
-        this.searchRange = 2
-      }
-      // 検索半径1000m以内なら
-      else if (this.radius * 1000 <= 1000) {
-        this.searchRange = 3
-      }
-      // 検索半径2000m以内なら
-      else if (this.radius * 1000 <= 2000) {
-        this.searchRange = 4
-      }
-      // 検索半径3000m以内なら
-      else if (this.radius * 1000 <= 3000) {
-        this.searchRange = 5
-      }
-      const data = await fetch(
-        this.url +
-          this.API_KEY +
-          "&lat=" +
-          this.current_location.lat +
-          "&lng=" +
-          this.current_location.lng +
-          "&range=" +
-          this.searchRange +
-          "&format=" +
-          this.format
-      )
-      const json = await data.json()
-      this.restaurants = json.results.shop
-    },
-    // 検索結果を取得する関数
-    getResults: function () {},
-
-    // とある2点間の距離を計算する関数
-    distance: function (lat1, lng1, lat2, lng2) {
-      const R = Math.PI / 180
-      const EARTH_RADIUS = 6371
-      lat1 *= R
-      lng1 *= R
-      lat2 *= R
-      lng2 *= R
-      return (
-        EARTH_RADIUS *
-        Math.acos(
-          Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) +
-            Math.sin(lat1) * Math.sin(lat2)
-        )
-      )
-    },
-    searchResturant: async function () {
-      this.results = []
-      await this.getLocation()
-      await this.getRestaurant()
-      for (let i = 0; i < this.restaurants.length; i++) {
-        // 検索半径なら検索結果とする
-        if (
-          this.radius >=
-          this.distance(
-            this.current_location.lat,
-            this.current_location.lng,
-            this.restaurants[i].lat,
-            this.restaurants[i].lng
-          )
-        ) {
-          this.results.push({
-            info: this.restaurants[i],
-            distance: this.distance(
-              this.current_location.lat,
-              this.current_location.lng,
-              this.restaurants[i].lat,
-              this.restaurants[i].lng
-            ),
-          })
-        }
-      }
+    // 詳細画面に遷移するための関数
+    screenTransition: function (index) {
+      this.$emit("detail", index)
+      this.$router.push("/detail")
     },
   },
 }
 </script>
+
+<style lang="scss">
+@mixin min-width($width: 481px) {
+  @media screen and (min-width: $width) {
+    @content;
+  }
+}
+
+@mixin min-max-width($min: 481px, $max: 800px) {
+  @media screen and (min-width: $min) and (max-width: $max) {
+    @content;
+  }
+}
+
+@mixin max-width($width: 480px) {
+  @media screen and (max-width: $width) {
+    @content;
+  }
+}
+
+.results-zone {
+  padding-left: 20px;
+  padding-right: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  .shop-zone {
+    overflow: hidden;
+    border-radius: 8px;
+    box-shadow: 0 4px 15px rgba(0, 0, 0, 0.2);
+    margin-right: 10px;
+    margin-left: 10px;
+    margin-bottom: 20px;
+    @include max-width(520px) {
+      width: calc(100%);
+    }
+    @include min-max-width(521px, 960px) {
+      width: calc(50% - 20px);
+    }
+    @include min-width(961px) {
+      width: calc(25% - 20px);
+    }
+    h3 {
+      padding-left: 20px;
+      padding-right: 20px;
+    }
+    img {
+      padding-top: 0;
+      height: 200px;
+      width: 100%;
+      margin-bottom: 10px;
+    }
+  }
+  .shop-zone:hover {
+    transform: translateY(-3px);
+    box-shadow: 0 7px 14px rgba(50, 50, 93, 0.1), 0 3px 6px rgba(0, 0, 0, 0.08);
+    transition: all 0.5s;
+  }
+}
+</style>
